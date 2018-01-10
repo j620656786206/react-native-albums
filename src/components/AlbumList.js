6dedb11cad13ecd05c66cafad9ca3daf01dfdba3
Alex Yu <j620656786206@gmail.com>
2018-01-10 10:13:00 -0600

    36. More on Component Level State & 37. Rendering a List of Components & 38. Displaying Individual Albums & 39. Fantastic Reusable Components - The Card & 40. Styling a Card & 41. Passing Components as Props & 42. Dividing Cards into Sections

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
   state = { albums: [] };
@@ -9,13 +10,18 @@ class AlbumList extends Component {
     axios.get('https://rallycoding.herokuapp.com/api/music_albums')
           .then(response => this.setState({ albums: response.data }));
   }
+  
+  renderAlbums() {
+    return this.state.albums.map(album => 
+      <AlbumDetail key={album.title} album={album} />);
+  }
 
   render() {
     console.log(this.state);
 
     return (
       <View>
-				<Text>Album List</Text>
+				{this.renderAlbums()}
       </View>
     );
   }
